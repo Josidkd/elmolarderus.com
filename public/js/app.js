2745f185a14a7d7b87e2af9a473c68b261b945b2
Giorgix <jackosinapsis@gmail.com>
2015-04-30 15:26:19 +0200

    added ajax and no-ajax rendering

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,22 @@
 $(function(){
-    $(".tall, .normal").mousedown(function(){       
-        $(this).addClass("selected");
-    });
- 
-    $(".tall, .normal").mouseup(function(){     
-        $(this).removeClass("selected");
-    });
+    
+  function renderTemplate(template) {
+       $.ajax({
+        type: 'GET',
+        url: template,
+        success: function (data) {
+          $(".content").html(data);
+        }
+      })
+    }
 
    $( document ).on( "click", "#products-link", function(e) {
       e.preventDefault();
-      $.ajax({
-        type: 'GET',
-        url: '/products/',
-        success: function (data) {
-          //Assuming the returned data is pure HTML
-          $("nav").replaceWith(data);
-        }
-      })
-    });
+      renderTemplate('/products');
+   });
+
    $( document ).on( "click", "a.button--close", function(e) {
-      console.log('hola');
       e.preventDefault();
-      $.ajax({
-        type: 'GET',
-        url: '/',
-        success: function (data) {
-          //Assuming the returned data is pure HTML
-          $("section").replaceWith(data);
-        }
-      })
-    });
+      renderTemplate('/');
+   });
 });
