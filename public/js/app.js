23d7bde0bb9c243ae67f85ed50568de59e889c83
Giorgix <jackosinapsis@gmail.com>
2015-04-30 15:08:30 +0200

    added ajax and no-ajax rendering

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,27 @@ $(function(){
         $(this).removeClass("selected");
     });
 
-    //$("#products-link").click(function() {
-      //$("#products").show();
-    //});
-    
-    //$(".button--close").click(function() {
-      //$(this).closest("section").hide();
-    //});
+   $( document ).on( "click", "#products-link", function(e) {
+      e.preventDefault();
+      $.ajax({
+        type: 'GET',
+        url: '/products/',
+        success: function (data) {
+          //Assuming the returned data is pure HTML
+          $("nav").replaceWith(data);
+        }
+      })
+    });
+   $( document ).on( "click", "a.button--close", function(e) {
+      console.log('hola');
+      e.preventDefault();
+      $.ajax({
+        type: 'GET',
+        url: '/',
+        success: function (data) {
+          //Assuming the returned data is pure HTML
+          $("section").replaceWith(data);
+        }
+      })
+    });
 });
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 
+function render(template, req, res) {
+  if ( req.headers['x-requested-with'] == 'XMLHttpRequest') {
+    res.render(template, {layout: null});
+  }
+  else {
+    res.render(template);
+  };
+
+}
+
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  render('index', req, res); 
 });
 
-router.get('/products', function(req, res) {
-  res.render('products');
+router.get('/products/', function(req, res) {
+   render('products', req, res); 
 });
 
 module.exports = router;
