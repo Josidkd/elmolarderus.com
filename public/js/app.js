4a60f94539b9addc3030ff08b1c060dad74059c0
Giorgix <jackosinapsis@gmail.com>
2015-05-01 16:30:53 +0200

    fixed mobile first and reorder with absolute position

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,33 @@
 $(function(){
-  $(document).pjax('a', '.container')
+  // we could use this library
+  $(document).pjax('a[data-pjax]', '.container')
+
+  // or try to do something like this
+  /*function renderTemplate(template) {
+   $.ajax({
+      type: 'GET',
+        url: template,
+        success: function (data) {
+          //Assuming the returned data is pure HTML
+          $(".container").html(data);
+          history.pushState({'name': template}, '', template);
+        }
+    })
+
+  }
+  function clickAndRender(element, template) {
+    $(document).on('click', element, function(e) {
+      var href = $(this).attr("href");
+      e.preventDefault();
+      renderTemplate(template);
+     });
+  }
+  clickAndRender('#products-link', '/products');
+  clickAndRender('.button--close', '/');
+  $(window).on("popstate", function () {
+    // if the state is the page you expect, pull the name and load it.
+    if (history.state) {
+      renderTemplate(history.state.name);
+    }
+});*/
 });
